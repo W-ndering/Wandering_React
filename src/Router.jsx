704ff450105675aa7e8d4122d9d7d8d7e56826ee
cykimbb <cykimbb@gmail.com>
2025-10-29 14:44:04 +0900

    여행자 만남

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Mountain from "./Mountain"
+import Mountain from "./Mountain";
+import Traveler from "./Traveler";
 // import Onboarding from "./pages/Onboarding";
 
 const Router = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Mountain />} />
+      <Route path="/traveler" element={<Traveler />} />
     </Routes>
   </BrowserRouter>
 );
