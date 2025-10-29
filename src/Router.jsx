2280a1b23f0fa6f99b6373159d792f6a675ffe94
cykimbb <cykimbb@gmail.com>
2025-10-29 16:40:33 +0900

    바닷가

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Onboarding from "./pages/Onboarding";
+import Beach from "./Beach"
+// import Onboarding from "./pages/Onboarding";
 
 const Router = () => (
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Onboarding />} />
+      <Route path="/beach" element={<Beach />} />
     </Routes>
   </BrowserRouter>
 );
