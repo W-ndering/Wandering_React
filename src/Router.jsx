bbbef2676b1b84d8fe86941c2edda4a8e845bec9
cykimbb <cykimbb@gmail.com>
2025-10-29 15:24:36 +0900

    하강

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Mountain from "./Mountain";
 import Traveler from "./Traveler";
+import ClimbDown from "./ClimbDown";
 // import Onboarding from "./pages/Onboarding";
 
 const Router = () => (
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Mountain />} />
+      <Route path="/mountain" element={<Mountain />} />
       <Route path="/traveler" element={<Traveler />} />
+      <Route path="/climbdown" element={<ClimbDown />} />
     </Routes>
   </BrowserRouter>
 );
