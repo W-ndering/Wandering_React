8aa3911910734e2f6b8199c32134f20ae3eae585
benscookie <ehrud864@icloud.com>
2025-10-30 13:28:59 +0900

    feat: Update Router for Credits branch

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,12 +1,13 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Onboarding from "./pages/Onboarding";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import Credits from "./Credits";
 
 const Router = () => (
-  <BrowserRouter>
+  <HashRouter>
     <Routes>
-      <Route path="/" element={<Onboarding />} />
+      <Route path="/" element={<Credits />} />
+      <Route path="/credits" element={<Credits />} />
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 export default Router;
